5b28e4ee3e352a84a493af035b057263be675a04
Cuong Nguyen <cuongnq@minori.com.vn>
2020-08-18 15:44:16 +0700

    Update nerd font

--- install.sh
+++ install.sh
@@ -41,7 +41,7 @@ cp -rf ./.config ~/
 
 echo "Install Fira Code Regular Nerd Font Complete Mono.ttf"
 mkdir -p ~/.local/share/fonts
-cd ~/.local/share/fonts && curl -fLo "Fira Code Regular Nerd Font Complete Mono.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+cd ~/.local/share/fonts && curl -fLo "Fira Code Regular Nerd Font Complete Mono.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete%20Mono.ttf
 
 cp ./.zshrc ~/.zshrc
 echo 'Install completed!'
