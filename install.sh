153f121f8832c7eeeb93f20584858fc064d56690
Cuong Nguyen <cuongnq@minori.com.vn>
2020-08-24 09:35:12 +0700

    Update font and tmux theme

--- install.sh
+++ install.sh
@@ -41,7 +41,7 @@ cp -rf ./.config ~/
 
 echo "Install Fira Code Regular Nerd Font Complete Mono.ttf"
 mkdir -p ~/.local/share/fonts
-cd ~/.local/share/fonts && curl -fLo "Fira Code Regular Nerd Font Complete Mono.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete%20Mono.ttf
+cd ~/.local/share/fonts && curl -fLo "Fira Code Regular Nerd Font Complete Mono.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete%20Mono.otf
 
 cp ./.zshrc ~/.zshrc
 echo 'Install completed!'
