f8094a87f8007c0990f7ba0fc61d3bb9a29d0980
Cuong Nguyen <cuongnq@minori.com.vn>
2020-08-27 19:30:33 +0700

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 starship init fish | source
 
-set PATH ~/.nvm/versions/node/v12.18.3/bin ~/.cargo/bin /snap/bin $PATH
+set PATH ~/.nvm/versions/node/v12.18.3/bin ~/.cargo/bin /snap/bin /usr/local/sbin /usr/sbin /sbin $PATH
 set fish_greeting
 set TERM tmux-256color
 
