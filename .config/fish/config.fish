badc7c0575ab4241766940604beb664ed88e539e
QuoCuonG <quoccuong15298@gmail.com>
2020-08-30 21:14:58 +0700

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,6 @@
-starship init fish | source
-
 set PATH ~/.nvm/versions/node/v12.18.3/bin ~/.cargo/bin /snap/bin /usr/local/sbin /usr/sbin /sbin $PATH
 set fish_greeting
 set TERM tmux-256color
 
+starship init fish | source
 zoxide init fish | source
