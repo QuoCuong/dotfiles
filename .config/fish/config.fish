6658123e07a48b3de4b5a4cc1bacb4d9cda76fd9
Cuong Nguyen <cuongnq@minori.com.vn>
2020-08-30 21:28:16 +0700

    Add vi function and update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,11 @@
 set PATH ~/.nvm/versions/node/v12.18.3/bin ~/.cargo/bin /snap/bin /usr/local/sbin /usr/sbin /sbin $PATH
 set fish_greeting
 set TERM tmux-256color
+set EDITOR nvim
+
+if test -n "$DESKTOP_SESSION"
+    set (gnome-keyring-daemon --start | string split "=")
+end
 
 starship init fish | source
 zoxide init fish | source
