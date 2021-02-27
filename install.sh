46fe7cdefc118dac7a78c556fdad9f3b0b965811
Cuong Nguyen <quoccuong15298@gmail.com>
2021-02-27 12:10:03 +0700

    Arco Linux Configuration

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/sh
 
 echo "Installing..."
-pacman -S base-devel yay conky tmux
+pacman -S tmux
+
+echo "Installing fira code nerd font..."
+yay -S nerd-fonts-fira-code
+
+echo "Installing VIM PLUG..."
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 echo "Copy config files"
 cp -r ./.config/* ~/.config
