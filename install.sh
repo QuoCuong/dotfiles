fdd06a9a7557295d1c3b139fe8f0382f10f815c4
Cuong Nguyen <quoccuong15298@gmail.com>
2021-03-03 08:49:42 +0700

    i3: Update config

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 
 echo "Installing..."
-pacman -S tmux python python-pip
+pacman -S tmux python python-pip xclip docker docker-compose
 
 echo "Installing pynvim module..."
 python -m pip install pynvim
