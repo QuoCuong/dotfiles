2908cec589773d447660eb17455a45e539d63937
QuoCuonG <quoccuong15298@gmail.com>
2021-04-21 14:51:16 +0700

    archlabs: fish, i3, polybar and dunst configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,4 +3,9 @@ set fish_greeting
 set TERM tmux-256color
 set EDITOR nvim
 
-zoxide init fish | source
+# Start X at login
+if status is-interactive
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec startx -- -keeptty
+    end
+end
