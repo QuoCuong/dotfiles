e6dfa332a939006dff7b50256dd4eb84eaf6660c
Cuong Nguyen <quoccuong15298@gmail.com>
2021-06-26 13:54:31 +0700

    archlabs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
-set PATH ~/.nvm/versions/node/v12.18.3/bin ~/.cargo/bin /snap/bin /usr/local/sbin /usr/sbin /sbin $PATH
+set PATH ~/.cargo/bin /snap/bin /usr/local/sbin /usr/sbin /sbin $PATH
 set fish_greeting
 set TERM tmux-256color
 set EDITOR nvim
--- install.sh
+++ install.sh
@@ -1,7 +1,37 @@
-#!/usr/bin/sh
+#!/usr/bin/fish
 
 echo "Installing..."
-pacman -S base-devel yay conky tmux
+sudo pacman -S base-devel fish tmux curl
 
-echo "Copy config files"
-cp -r ./.config/* ~/.config
+echo ">> Copy config files"
+sudo cp -r ./.config/* ~/.config
+sudo cp -r .tmux.conf ~/
+
+#echo ">> Fisher: Installing"
+#source $(curl -sL https://git.io/fisher) && fisher install jorgebucaran/fisher
+
+echo ">> Fisher: Installing nvm plugin"
+fisher install jorgebucaran/nvm.fish
+
+echo ">> Fisher: Installing pure prompt"
+fisher install pure-fish/pure
+
+echo ">> Polybar: Installing"
+yay -S polybar
+
+echo ">> Fonts: Installing FiraCode Nerd Font"
+yay -S nerd-fonts-fira-code
+
+echo ">> Neovim: Installing"
+sudo pacman -S neovim
+
+echo ">> VimPlug: Installing"
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+echo ">> NVM: Installing LTS"
+nvm install lts
+set -gx PATH (which node) $PATH
+
+echo "Neovim: PlugInstall"
+nvim --headless +PlugInstall +qa
