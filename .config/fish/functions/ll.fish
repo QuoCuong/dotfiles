db234a1bca515d8387a3f3c47f607fe370af5327
Cuong Nguyen <quoccuong15298@gmail.com>
2022-02-13 19:06:49 +0700

    update fish functions

--- .config/fish/functions/ll.fish
+++ .config/fish/functions/ll.fish
@@ -1,3 +1,3 @@
-function ll
-	exa -l --color always --icons -a -s type
+function ll --wraps=ls --wraps='exa -l --color always --icons -a -s type' --description 'alias ll=exa -l --color always --icons -a -s type'
+  exa -l --color always --icons -a -s type $argv; 
 end
--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,3 +1,3 @@
-function ls
-	exa -G  --color auto --icons -a -s type
+function ls --wraps='exa -l --color always --icons -a -s type' --description 'alias ls=exa -l --color always --icons -a -s type'
+  exa -l --color always --icons -a -s type $argv; 
 end
