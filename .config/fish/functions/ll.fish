1422f6a680e6ce862bb6304e5cffc33de2b0b376
Cuong Nguyen <cuongnq@meta-technology.com.vn>
2023-02-10 10:41:47 +0700

    new alias

--- .config/fish/functions/ll.fish
+++ .config/fish/functions/ll.fish
@@ -1,3 +1,4 @@
-function ll
-	exa -l --color always --icons -a -s type
+function ll --wraps='exa -l --color always --icons -a -s type' --description 'alias ll=exa -l --color always --icons -a -s type'
+  exa -l --color always --icons -a -s type $argv
+        
 end
--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,3 +1,4 @@
-function ls
-	exa -G  --color auto --icons -a -s type
+function ls --wraps='exa -G --color auto --icons -a -s type' --description 'alias ls=exa -G --color auto --icons -a -s type'
+  exa -G --color auto --icons -a -s type $argv
+        
 end
